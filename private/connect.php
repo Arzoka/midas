24431d05c9381fac7feb152e40ff8377d878780c
Arzoka <114647680+Arzoka@users.noreply.github.com>
2023-06-06 10:17:36 +0200

    products page connect fix

--- private/connect.php
+++ private/connect.php
@@ -1,28 +1,11 @@
 <?php
-if (!empty($_POST)) {
-  if(isset($_POST['standard'])){
-    $sql = "SELECT * FROM items ORDER BY item_id asc";
-  }
-  if(isset($_POST['highlow'])){
-    $sql = "SELECT * FROM items ORDER BY item_price desc";
-  }
-  if(isset($_POST['lowhigh'])){
-    $sql = "SELECT * FROM items ORDER BY item_price asc";
-  }
-  if(isset($_POST['az'])){
-    $sql = "SELECT * FROM items ORDER BY item_name asc";
-  }
-    
-}
 
-else {
-	$sql = "SELECT * FROM items ORDER BY item_id asc";
-}
-
+$mysqli = mysqli_connect("localhost:3307", "root", "", "items");
+  
 $sql = "SELECT * FROM items ORDER BY item_id asc";
+$result = $mysqli->query($sql);
 
-$conn = mysqli_connect("localhost:3307", "root", "", "items");
-$result = $conn->query($sql);
+
 
 if ($result->num_rows > 0) {
 	while ($row = $result-> fetch_assoc()) {
@@ -36,7 +19,5 @@ if ($result->num_rows > 0) {
 		}
 	}
 }
-
-$conn->close();
 
 ?>
